f180c137818a8c0822446715a38658fe9135541b
ArunRamakani <ArunRamakani@users.noreply.github.com>
2014-12-18 17:40:04 +0400

    Initial Commit
    Initial Commit

---
+++ PacteraAssessment/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  PacteraAssessment
+//
+//  Created by Arun Ramakani on 12/18/14.
+//  Copyright (c) 2014 Pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) ViewController *baseView;
+
+@end
+
---
+++ PacteraAssessment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PacteraAssessment
+//
+//  Created by Arun Ramakani on 12/18/14.
+//  Copyright (c) 2014 Pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
